fe8373511046a92bfa33647b9f7417c975731fd8
Gaurav Hegade <neph005@protonmail.com>
2024-03-05 19:50:14 +0530

    Add and integrate DPDA functionality with existing app

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
 from flask import Flask, render_template, request
-from visualize import PushDownAutomata as pda
+from visualize import determine_pushdown_automaton as determine_pushdown_automaton
+from visualize import (
+    nondeterministic_pushdown_automaton as nondeterministic_pushdown_automaton,
+)
 from html2text import html2text
 import ast
 
 app = Flask("PDA Visualizer")
 
 
-@app.route("/", methods=["POST", "GET"])
-def home():
+@app.route("/npda", methods=["POST", "GET"])
+def npda():
     if request.method == "POST":
         # get the states in the automata
         states = request.form.get("input_states")
@@ -81,7 +84,7 @@ def home():
         #     acceptance_mode,
         # )
 
-        npda = pda.create_npda(
+        pda = nondeterministic_pushdown_automaton.create_npda(
             states,
             input_symbols,
             stack_symbols,
@@ -92,7 +95,7 @@ def home():
             acceptance_mode,
         )
 
-        if pda.validate_npda(npda):
+        if nondeterministic_pushdown_automaton.validate_npda(pda):
             print("NPDA validated")
 
         else:
@@ -101,16 +104,15 @@ def home():
         res = []
         if test_strings is not None:
             for test_string in test_strings:
-                res.append(pda.simulate_npda(test_string, npda))
+                res.append(
+                    nondeterministic_pushdown_automaton.simulate_npda(test_string, pda)
+                )
 
         else:
             pass
 
-        # TODO: FIND A BETTER WAY TO PRINT THE RESULT
-        # ONE WAY: ATTACH THE TEST CASE RESULT TO THE TEST CASE ITSELF AND USE IT IN PRINTING THE DIAGRAMS
-        # ANOTHER WAY: NEED TO BE THOUGHT OF
-
         context = {
+            "mode": "NPDA",
             "npda": "static/npda/diagram.png",
             "result": res,
             "test_strings": test_strings,
@@ -118,7 +120,121 @@ def home():
 
         return render_template("index.html", context=context)
 
-    return render_template("index.html")
+    return render_template("index.html", mode="NPDA")
+
+
+@app.route("/dpda", methods=["POST", "GET"])
+def dpda():
+    if request.method == "POST":
+        # get the states in the automata
+        states = request.form.get("input_states")
+        states = states.replace(" ", "")
+        states = set(states.split(",")) if states else None
+
+        # get the input symbols in the automata
+        input_symbols = request.form.get("input_alphabets")
+        input_symbols = input_symbols.replace(" ", "")
+        input_symbols = set(input_symbols.split(",")) if input_symbols else None
+
+        # get the stack symbols in the automata
+        stack_symbols = request.form.get("stack_symbols")
+        stack_symbols = stack_symbols.replace(" ", "")
+        stack_symbols = set(stack_symbols.split(",")) if stack_symbols else None
+
+        # get the transitions of the automata in json format
+        transitions_str = request.form.get("transitions")
+        transitions_str = html2text(transitions_str)
+        try:
+            transitions = ast.literal_eval(transitions_str)
+
+        except (ValueError, SyntaxError) as e:
+            print(f"Error parsing input: {e}")
+
+        initial_state = request.form.get("initial_state")
+        initial_state = initial_state.replace(" ", "") if initial_state else None
+
+        initial_stack_symbol = request.form.get("initial_stack_symbol")
+        initial_stack_symbol = (
+            initial_stack_symbol.replace(" ", "") if initial_stack_symbol else None
+        )
+
+        final_states = request.form.get("final_states")
+        final_states = final_states.replace(" ", "")
+        final_states = set(final_states.split(",")) if final_states else None
+
+        acceptance_mode = request.form.get("acceptance_mode")
+
+        # # Get input strings to test from user
+        test_strings = request.form.get("test_strings")
+        test_strings = test_strings.replace(" ", "")
+        test_strings = list(test_strings.split(",")) if test_strings else None
+
+        states = states
+        input_symbols = input_symbols
+        stack_symbols = stack_symbols
+        transitions = transitions
+        initial_state = initial_state
+        initial_stack_symbol = initial_stack_symbol
+        final_states = final_states
+        acceptance_mode = acceptance_mode
+
+        # # Debugging
+        # print(
+        #     "\n STATES \n",
+        #     states,
+        #     "\n INPUT SYMBOLS \n",
+        #     input_symbols,
+        #     "\n STACK SYMBOLS \n",
+        #     stack_symbols,
+        #     "\n TRANSITIONS \n",
+        #     transitions,
+        #     "\n INITIAL STATE \n",
+        #     initial_state,
+        #     "\n INITIAL STACK SYMBOL \n",
+        #     initial_stack_symbol,
+        #     "\n FINAL STATES \n",
+        #     final_states,
+        #     "\n ACCEPTANCE MODE \n",
+        #     acceptance_mode,
+        # )
+
+        pda = determine_pushdown_automaton.create_dpda(  # Use create_dpda instead of create_npda
+            states,
+            input_symbols,
+            stack_symbols,
+            transitions,
+            initial_state,
+            initial_stack_symbol,
+            final_states,
+            acceptance_mode,
+        )
+
+        if determine_pushdown_automaton.validate_dpda(
+            pda
+        ):  # Use validate_dpda instead of validate_npda
+            print("DPDA validated")  # Update message
+
+        else:
+            print("DPDA not validated")  # Update message
+
+        res = []
+        if test_strings is not None:
+            for test_string in test_strings:
+                res.append(determine_pushdown_automaton.simulate_dpda(test_string, pda))
+
+        else:
+            pass
+
+        context = {
+            "mode": "DPDA",
+            "dpda": "static/dpda/diagram.png",  # Update path
+            "result": res,
+            "test_strings": test_strings,
+        }
+
+        return render_template("index.html", context=context)
+
+    return render_template("index.html", mode="DPDA")
 
 
 if __name__ == "__main__":
--- visualize.py
+++ visualize.py
@@ -1,10 +1,11 @@
 from automata.pda.npda import NPDA
+from automata.pda.dpda import DPDA
 from automata.base.exceptions import RejectionException
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 
 
-class PushDownAutomata:
+class nondeterministic_pushdown_automaton:
     def create_npda(
         input_states,
         input_alphabets,
@@ -48,6 +49,57 @@ class PushDownAutomata:
             if input_string == "E":
                 input_string = ""
             npda.show_diagram(input_str=input_string, path=plot_path, with_stack=True)
+            if input_string == "":
+                input_string = "E"
+            return True, input_string, f"{input_string} is accepted!"
+        else:
+            return False, input_string, f"{input_string} is not accepted!"
+
+
+class determine_pushdown_automaton:
+    def create_dpda(
+        input_states,
+        input_alphabets,
+        input_stack_alphabets,
+        input_transitions,
+        input_initial_state,
+        input_initial_stack_symbol,
+        input_final_states,
+        input_acceptance_mode,
+    ):
+        dpda = DPDA(
+            states=input_states,
+            input_symbols=input_alphabets,
+            stack_symbols=input_stack_alphabets,
+            transitions=input_transitions,
+            initial_state=input_initial_state,
+            initial_stack_symbol=input_initial_stack_symbol,
+            final_states=input_final_states,
+            acceptance_mode=input_acceptance_mode,
+        )
+        return dpda
+
+    def validate_dpda(dpda):  # Rename validate_npda to validate_dpda
+        if dpda.validate():
+            return False
+        else:
+            dpda.show_diagram(path="static/dpda/diagram.png")  # Update path
+            return True
+
+    def simulate_dpda(input_string, dpda):  # Rename simulate_npda to simulate_dpda
+        if input_string == "E":
+            input_string = ""
+
+        is_accepted = dpda.accepts_input(input_string)
+
+        if input_string == "":
+            input_string = "E"
+        plot_path = f"static/dpda/tests/test-{input_string}.png"  # Update path
+
+        if is_accepted:
+            if input_string == "E":
+                input_string = ""
+            dpda.show_diagram(input_str=input_string, path=plot_path, with_stack=True)
             if input_string == "":
                 input_string = "E"
             return True, input_string, f"{input_string} is accepted!"
